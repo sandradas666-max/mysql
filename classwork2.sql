96718f6761326ea1a211f697bce5ace0ad650707
sandradas666-max <sandradas.666@gmail.com>
2026-09-16 15:58:49 +0530

    Add SQL scripts for creating and populating students and books tables

---
+++ classwork2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `college_db`.`students` (`id` INT NOT NULL , `name` VARCHAR(30) NOT NULL , `age` INT NOT NULL , `department` VARCHAR(30) NOT NULL , `grade` INT NOT NULL ) ENGINE = InnoDB;
+INSERT INTO students(id,name,age,department,grade) VALUES(1,"sana",20,"physics",90), (2,"manu",19,"computer science",80), (3,"arun",22,"chemistry",60), (4,"veena",23,"physics",76);
+
+SELECT * FROM students;
+SELECT * FROM students WHERE age>20;
+SELECT * FROM students;
+SELECT * FROM students WHERE department IN('computer science','physics');
+SELECT * FROM students WHERE grade=90;
+SELECT * FROM students WHERE grade BETWEEN 70 AND 90;
