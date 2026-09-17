4d4c9fc91ed0061594cd23f5f4f860195c803797
sandradas666-max <sandradas.666@gmail.com>
2026-09-17 12:41:46 +0530

    Refactor SQL table creation for students and books to improve readability

--- classwork2.sql
+++ classwork2.sql
@@ -1,4 +1,9 @@
-CREATE TABLE `college_db`.`students` (`id` INT NOT NULL , `name` VARCHAR(30) NOT NULL , `age` INT NOT NULL , `department` VARCHAR(30) NOT NULL , `grade` INT NOT NULL ) ENGINE = InnoDB;
+CREATE TABLE students
+(
+    id INT NOT NULL , 
+    name VARCHAR(30) NOT NULL , 
+    age INT NOT NULL , department VARCHAR(30) NOT NULL , 
+    grade INT NOT NULL );
 INSERT INTO students(id,name,age,department,grade) VALUES(1,"sana",20,"physics",90), (2,"manu",19,"computer science",80), (3,"arun",22,"chemistry",60), (4,"veena",23,"physics",76);
 
 SELECT * FROM students;
--- homework2.sql
+++ homework2.sql
@@ -1,4 +1,7 @@
-CREATE TABLE `college_db`.`books` (`id` INT NOT NULL , `title` VARCHAR(30) NOT NULL , `author` VARCHAR(30) NOT NULL , `price` INT NOT NULL , `genre` VARCHAR(30) NOT NULL ) ENGINE = InnoDB;
+CREATE TABLE college_db .books (id INT NOT NULL , 
+title VARCHAR(30) NOT NULL , author VARCHAR(30) NOT NULL ,
+ price INT NOT NULL , genre  VARCHAR(30) NOT NULL );
+
 INSERT INTO books(id,title,author,price,genre) VALUES(1,"the great gatsby","dan brown",350,"fiction"), (2,"sapiens","yuval noah harai",550,"history"), (3,"a brief history of time","stephen hawking",600,"science"), (4,"the alchemist","paulo coelho",300,"fiction"), (5,"the silk roads","peter frankopan",450,"history");
 SELECT * FROM books
 SELECT * FROM books WHERE price > 400;
