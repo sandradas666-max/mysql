00f18752bcdfadb15405ce1acb3586ab818053d8
sandradas666-max <sandradas.666@gmail.com>
2026-09-25 13:42:34 +0530

    Enhance SQL scripts by adding primary keys to authors and products tables

--- classwork8.sql
+++ classwork8.sql
@@ -1,6 +1,6 @@
 create database grocery_shop;
  use grocery_shop;
- create table products(product_id int,product_name varchar(30),price int);
+ create table products(product_id int primary key,product_name varchar(30),price int);
  alter table products add category varchar(30);
   delete from products;
-   drop table products;
+   drop database grocery_shop;
--- homework8.sql
+++ homework8.sql
@@ -1,7 +1,7 @@
 create database BookStoreDB;
  use BookStoreDB;
- create table authors(author_id int,name varchar(30),country varchar(30));
- create table books(book_id int,title varchar(30),price int,author_id int);
+ create table authors(author_id int primary key,name varchar(30),country varchar(30));
+ create table books(book_id int primary key,title varchar(30),price int,author_id int foreign key(author_id)references authors(author_id));
  alter table books add published_year int;
  delete from books;
   drop database BookStoreDB;
